c5ef5f0997f11659cc01c5d59ad8bd539faf7982
TanjaCirkovic <tanjacirkovic0@gmail.com>
2026-03-10 14:49:07 +0100

    Adding two pages: customer_service and information.

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 
 # My E-Commerce Project 
 This project is a simple online store to practice HTML, CSS, and JavaScript. 
-
->>>>>>> 
+ 
---
+++ js/customer_s.js
@@ -0,0 +1,6 @@
+
+
+function faqToggle(faq_id) {
+    const answer = document.getElementById(faq_id);
+    answer.style.display == "block" ? answer.style.display = "none" : answer.style.display = "block";
+    }
